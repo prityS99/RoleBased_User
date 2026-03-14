9a5100532c414cead4475575a1d9a6c392965e46
prityS99 <99pritys@gmail.com>
2026-03-14 16:46:06 +0530

    add

---
+++ add/client/src/compo/RecordCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RecordCard() {
+  return (
+    <div>RecordCard</div>
+  )
+}
+
+export default RecordCard
---
+++ add/client/src/compo/RecordForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RecordForm() {
+  return (
+    <div>RecordForm</div>
+  )
+}
+
+export default RecordForm
